5e91c74e9dfdbadb51e560a03143dfb7697bd167
James Alwin A. Aljecera <142960023+aws-jamesalwin-aljecera@users.noreply.github.com>
2023-09-21 15:26:53 +0800

    Update streamlit_app.py
    Adjusting put the new code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 import pandas
-import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -19,4 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
