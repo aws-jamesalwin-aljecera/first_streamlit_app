52e9c95c26df128b9eb3bed2cc9e3b99ff4c8519
James Alwin A. Aljecera <142960023+aws-jamesalwin-aljecera@users.noreply.github.com>
2023-09-21 08:56:18 +0800

    Update streamlit_app.py
    Added filter to reflect the changes in the data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -12,5 +13,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Bananas'])
+streamlit.dataframe(fruits_to_show)
