dfc0f60e099a314d4a1b9be82c60c11e2e71444e
James Alwin A. Aljecera <142960023+aws-jamesalwin-aljecera@users.noreply.github.com>
2023-09-22 10:32:08 +0800

    Update streamlit_app.py
    Move import codes above and added insert SQL command

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
@@ -32,7 +35,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Will display as data frame (more like tabulation)
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -43,3 +45,4 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?',"")
 streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
